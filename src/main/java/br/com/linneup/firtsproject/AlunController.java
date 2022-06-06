6c6912172a57e942d11abdc4159c9db459e4872d
Douglas Oliveira <douglas94_oliveira@hotmail.com>
2022-06-05 23:25:55 -0300

    Modulo1Pro

--- src/main/java/br/com/linneup/firtsproject/AlunController.java
+++ src/main/java/br/com/linneup/firtsproject/AlunController.java
@@ -21,18 +21,26 @@ public class AlunController {
     }
 
     @GetMapping
-    public List<Message> findAll(@RequestParam(required = false) String aluno) {
+    public List<Message> findAll(@RequestParam(required = false) String aluno, Integer id, Integer idade) {
         if (aluno != null) {
-            return Alunos.stream().filter(msg -> msg.getAluno()
+            return Alunos.stream().filter(aln -> aln.getAluno()
                     .contains(aluno)).collect(Collectors.toList());
 
+        } else if (id != null) {
+            return Alunos.stream().filter(ida -> ida.getId()
+                    .equals(id)).collect(Collectors.toList());
+
+        } else if (idade != null) {
+            return Alunos.stream().filter(idades -> idades.getIdade()
+                    .equals(idade)).collect(Collectors.toList());
         }
         return Alunos;
     }
 
+
     @GetMapping("/{id}")
     public Message findById(@PathVariable("id") Integer id) {
-        return this.Alunos.stream().filter(msg -> msg.getId().equals(id))
+        return this.Alunos.stream().filter(fiid -> fiid.getId().equals(id))
                 .findFirst().orElse(null);
     }
 
@@ -49,7 +57,7 @@ public class AlunController {
 
     public ResponseEntity<Integer> addIdade(@RequestBody final Message aluno) {
         if (aluno.getIdade() == null) {
-            aluno.setIdade(Alunos.size() + 10);
+            aluno.setIdade(Alunos.size() + 1);
         }
         Alunos.add(aluno);
         return new ResponseEntity<>(aluno.getIdade(), HttpStatus.CREATED);
@@ -57,16 +65,26 @@ public class AlunController {
 
     @PutMapping
     public ResponseEntity update(@RequestBody final Message aluno) {
-        Alunos.stream().filter(msg -> msg.getId().equals(aluno.getId()))
-                .forEach(msg -> msg.setAluno(aluno.getAluno()));
+        Alunos.stream().filter(upd -> upd.getId().equals(aluno.getId()))
+                .forEach(upd -> upd.setAluno(aluno.getAluno()));
+        Alunos.stream().filter(upda -> upda.getIdade().equals(aluno.getIdade()))
+                .forEach(upd -> upd.setIdade(aluno.getIdade()));
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable("id") Integer id) {
-        Alunos.removeIf(msg -> msg.getId().equals(id));
+        Alunos.removeIf(del -> del.getId().equals(id));
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
     }
 
+    @DeleteMapping("/{idade}")
+    public ResponseEntity deleteIdade(@PathVariable("idade") Integer idade) {
+        Alunos.removeIf(dele -> dele.getIdade().equals(idade));
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
 }
